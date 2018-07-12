e232bf22e7342e76486a4ca0ee13b33da84d8cab
apocalypeseed <tswlew@yahoo.com>
2018-07-11 20:35:28 -0400

    Initial commit

---
+++ src/main/java/com/thomas/springboot_008/Springboot008Application.java
@@ -0,0 +1,12 @@
+package com.thomas.springboot_008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot008Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot008Application.class, args);
+	}
+}
